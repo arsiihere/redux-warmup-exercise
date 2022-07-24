08970dbcf3ef9ba3974210cc00ea06c6e539d2bb
arsiihere <arsiiahmi@gmail.com>
2022-07-24 16:23:23 +0500

    Subscribe counter cmponent with redux store

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
