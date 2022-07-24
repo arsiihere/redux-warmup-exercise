d77a6bf55c48f7f84ef401fe1642cbfdaed46054
arsiihere <arsiiahmi@gmail.com>
2022-07-24 16:36:54 +0500

    add dynamic value counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,6 +11,11 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
@@ -19,6 +24,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
